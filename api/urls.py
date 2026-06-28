ddd10b6aca4fabece19116caa04803d8538fd21b
Ankit <akc9517557130@gmail.com>
2026-06-28 12:45:08 +0530

    PUT POST DELETE Learn

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('student/', views.studentViews),
-    
+    path("student/<int:pk>/", views.studentDetailViews)
+
 ]
